887fbc28599baa9db44a91c31db5193d7ba7f742
Somendra Pratap Singh <somenprtap.singh@gmail.com>
2022-03-11 20:24:51 +0530

    Commit

--- Pages/Account/Index.cshtml.cs
+++ Pages/Account/Index.cshtml.cs
@@ -1,13 +1,20 @@
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace ExampleIdentityApp.Pages.Account
 {
     public class IndexModel : PageModel
     {
+        private readonly UserManager<IdentityUser> _usermanager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+        public IndexModel(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            _usermanager = userManager;
+            _signInManager = signInManager;
+        }
         public void OnGet()
         {
-            
+
         }
     }
 }
