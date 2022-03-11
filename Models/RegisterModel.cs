65fb65f2c77d13a768de6b5450834670cdf66d6b
Somendra Pratap Singh <somenprtap.singh@gmail.com>
2022-03-11 10:16:37 +0530

    Add Model and Pages

---
+++ Models/RegisterModel.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace ExampleIdentityApp.Models
+{
+    public class RegisterModel
+    {
+        [Required]
+        public string Email { get; set; } = string.Empty;
+        [Required]
+        public string Password { get; set; } = string.Empty;
+        [Required]
+        public string ConfirmPassword { get; set; } = string.Empty;
+    }
+}
